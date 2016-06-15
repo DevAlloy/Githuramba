581bdfb384fa66552590d68c3e2b08f1e6cbbc32
Tsyganov Stanislav <s.tsyganov@rambler-co.ru>
2016-06-15 23:15:18 +0300

    Начальный коммит

---
+++ Githuramba/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  Githuramba
+//
+//  Created by Tsyganov Stanislav on 15/06/16.
+//  Copyright (c) 2016 DevAlloy. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
---
+++ Githuramba/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  Githuramba
+//
+//  Created by Tsyganov Stanislav on 15/06/16.
+//  Copyright (c) 2016 DevAlloy. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+
+}
