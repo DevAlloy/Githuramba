51ccfa987b8c89214eb2641b0572e30eea5b69aa
Tsyganov Stanislav <s.tsyganov@rambler-co.ru>
2016-06-15 23:16:17 +0300

    Добавлена модель репозитория

---
+++ Githuramba/Models/STVRepo.h
@@ -0,0 +1,17 @@
+//
+// Created by Tsyganov Stanislav on 15/06/16.
+// Copyright (c) 2016 DevAlloy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface STVRepo : NSObject
+
+@property (nonatomic, copy) NSString *identifier;
+@property (nonatomic, copy) NSString *description;
+@property (nonatomic, copy) NSDate *createdAt;
+@property (nonatomic, copy) NSNumber *stars;
+@property (nonatomic, copy) NSNumber *forks;
+
+@end
