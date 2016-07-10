113bfc202b333e0fb8cb8bc2337f7804886ca3d2
Tsyganov Stanislav <s.tsyganov@rambler-co.ru>
2016-07-10 17:04:25 +0300

    Добавлен bridge-файл для swift

---
+++ Githuramba/Githuramba-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Githuramba-Bridging-Header.h
+//  Githuramba
+//
+//  Created by Tsyganov Stanislav on 10/07/16.
+//  Copyright © 2016 DevAlloy. All rights reserved.
+//
+
+#ifndef Githuramba_Bridging_Header_h
+#define Githuramba_Bridging_Header_h
+
+#import "STVRepoService.h"
+#import "STVRamblerRepoServiceImplementation.h"
+#import "STVRepo.h"
+
+#endif /* Githuramba_Bridging_Header_h */
